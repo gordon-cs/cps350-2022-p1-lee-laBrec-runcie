904ec7aab252a5ef69e9ff1233cdd76e9c589fbe
Gavin <gavinlabrec@gmail.com>
2022-02-18 14:54:35 -0500

    Start working on navigation. Problem with what navigator contains

--- ExtremeWeatherApp/App.js
+++ ExtremeWeatherApp/App.js
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Weather from './src/Weather.js'
 
 export default function App() {
   return (
-    <View style={{flex:1,}}>
-      <ImageBackground source = {require('./src/gradientbackground.jpg')} resizeMode="cover" style = {{flex:1}}>
-          <Weather/>
-          <StatusBar hidden />
-      </ImageBackground>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <View style={{flex:1,}}>
+          <ImageBackground source = {require('./src/gradientbackground.jpg')} resizeMode="cover" style = {{flex:1}}>
+            <Weather/>
+            <StatusBar hidden />
+          </ImageBackground>
+        </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
