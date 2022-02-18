1a254bc3588c597206a1d0798231b5a5479cae48
Gavin <gavinlabrec@gmail.com>
2022-02-18 16:21:20 -0500

    Create Settings.js page. Does not work

--- ExtremeWeatherApp/App.js
+++ ExtremeWeatherApp/App.js
@@ -2,34 +2,49 @@ import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Weather from './src/Weather.js'
+
+import Weather from './src/Weather.js'; // Think I can remove
+import Homescreem from './scr/screens/Home.js';
+import Settingsscreen from './scr/screens/Settings.js';
+
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <View style={{flex:1,}}>
-          <ImageBackground source = {require('./src/gradientbackground.jpg')} resizeMode="cover" style = {{flex:1}}>
-            <Weather/>
-            <StatusBar hidden />
-          </ImageBackground>
-        </View>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppContainer />;
+  // return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+  // );
 }
 
-const Stack = createNativeStackNavigator();
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Homescreem
+ },
+ Settings: {
+  screen: Settingsscreen
+  }
+});
 
-const MyStack = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+const AppContainer = createAppContainer(AppNavigator);
+
+
+// https://blog.logrocket.com/navigating-react-native-apps-using-react-navigation/
+
+
+// const Stack = createNativeStackNavigator();
+
+// const MyStack = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
