9500d2dc7a0fd964fb760915f33b8f668d634ab5
Gavin <gavinlabrec@gmail.com>
2022-02-19 16:26:54 -0500

    Implement basic navigation structure

--- ExtremeWeatherApp/App.js
+++ ExtremeWeatherApp/App.js
@@ -1,50 +1,64 @@
+// import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Weather from './src/Weather.js'; // Think I can remove
-import HomeScreen from './src/screens/Home.js';
-import SettingsScreen from './src/screens/Settings.js';
-
-
-export default function App() {
-  return <AppContainer />;
-  // return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-  // );
-}
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Homescreen
- },
- Settings: {
-  screen: Settingsscreen
-  }
-});
-
-const AppContainer = createAppContainer(AppNavigator);
+// import Weather froms './src/Weather.js'; // Think I can remove
+//import HomeScreen from './src/screens/Home.js';
+// import SettingsScreen from './src/screens/Settings.js';
 
 
-// https://blog.logrocket.com/navigating-react-native-apps-using-react-navigation/
-
+// function HomeScreen() {
+//   return (
+//     <View >
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
 
 // const Stack = createNativeStackNavigator();
 
-// const MyStack = () => {
+// function App() {
 //   return (
 //     <NavigationContainer>
 //       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Home"
-//           component={HomeScreen}
-//         />
+//         <Stack.Screen name="Home" component={HomeScreen} />
 //       </Stack.Navigator>
 //     </NavigationContainer>
 //   );
-// };
+// }
+
+// export default App;
+
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Weather from './src/Weather.js';
+// import HomeScreen from './src/screens/Home.js';
+
+function HomeScreen() {
+  return (
+    <View style={{flex:1,}}>
+        <ImageBackground source = {require('./src/gradientbackground.jpg')} resizeMode="cover" style = {{flex:1}}>
+        <Weather/>
+        <StatusBar hidden />
+        </ImageBackground>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
