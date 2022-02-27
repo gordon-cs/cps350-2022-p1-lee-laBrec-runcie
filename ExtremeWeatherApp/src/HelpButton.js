e49f230d98cb9d4db944ba6da1481b214a110313
Gavin <gavinlabrec@gmail.com>
2022-02-27 00:57:53 -0500

    Add HelpButton alert

--- ExtremeWeatherApp/src/HelpButton.js
+++ ExtremeWeatherApp/src/HelpButton.js
@@ -1,12 +1,20 @@
 import { React, Component } from 'react';
-import { Image, Pressable, View } from 'react-native';
+import { Image, Pressable, View, Alert } from 'react-native';
 
 /* HelpButton class - no Props need to be provided*/
 export default class HelpButton extends Component {
   render() {
+    const helpText = 
+    "Danger score below based off of current weather conditions.";
+    const settingText = "Change location and proportion each \
+weather factor contributes to your score in the settings menu. \
+Press reload button after changing settings." 
     return (
       <View style={{flex: 1.5, paddingLeft:5}}>
-        <Pressable onPress={() => navigation.navigate('Settings')}>
+        <Pressable onPress={() => Alert.alert(
+          helpText,
+          settingText,
+        )}>
           <Image
             source={require('./helpicon.png')}
             style={{
